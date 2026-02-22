1d25f12c43f6596105600830fc0ce2fce9085ee0
polmar840-ops <polmar840@gmail.com>
2026-02-22 13:50:15 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '212722-313874',
+ pairingNumber : '212722-313874',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '212722-313874',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '722-313874,
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
